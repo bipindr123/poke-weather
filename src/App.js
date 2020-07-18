1779fcf13a705ae2785d6130279d08a6dd39a6a1
bipindr123@gmail.com <bipindr123@gmail.com>
2020-07-18 19:06:45 +0530

    inital coords input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bulma/bulma.sass";
+import "./App.scss";
 
-function App() {
+class PokeWeather extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      coords: '',
+      coords_submitted: false
+    };
+  }
+
+  validateCoords(coords) {
+    var coordRe = /^-?\d*.\d*,\s?-?\d*.\d*\n?$/
+    coords = coords.split("\n")
+    for (const index in coords) {
+      if (!(coordRe.test(coords[index])))
+        return false;
+    }
+    return true;
+  }
+
+  handleChange = (event) => {
+    this.setState({ coords: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    if (this.validateCoords(this.state.coords)) {
+      alert('Coords sumbmited please wait: ' + this.state.coords);
+      this.setState({coords_submitted: true})
+    }
+    else
+      alert('Wrong format');
+  event.preventDefault();
+}
+
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+
+      <h1 class="title is-1"> PokeWeather </h1>
+      <h2 class="subtitle"> Enter list of coords </h2>̥̥
+      <textarea class="textarea" placeholder="43.45651,-73.5615" value={this.state.coords} onChange={this.handleChange}></textarea>
+      <br></br>
+      <div class="field is-grouped">
+        <div class="control">
+          <button class="button is-link" onClick={this.handleSubmit}>SUBMIT</button>
+        </div>
+      </div>
+
     </div>
   );
 }
+}
 
+function App() {
+  return <PokeWeather />;
+}
 export default App;
